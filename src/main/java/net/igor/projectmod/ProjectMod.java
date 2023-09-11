ad699c5ffd91edefe44648b824c4a1efb24f74c2
Игорь Кудрявцев <kudriavtsiev_2004@mail.ru>
2023-09-11 12:39:47 +0300

    Test Commit

--- src/main/java/net/igor/projectmod/ProjectMod.java
+++ src/main/java/net/igor/projectmod/ProjectMod.java
@@ -12,15 +12,11 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(ProjectMod.MOD_ID)
 public class ProjectMod
 {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "projectmod";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
 
     public ProjectMod()
     {
