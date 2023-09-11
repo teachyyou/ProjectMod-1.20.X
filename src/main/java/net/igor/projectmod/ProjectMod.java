df17a6b00511b1b84a8a668c7fe0ede21d17c456
Игорь Кудрявцев <kudriavtsiev_2004@mail.ru>
2023-09-11 13:04:36 +0300

    New Item added

--- src/main/java/net/igor/projectmod/ProjectMod.java
+++ src/main/java/net/igor/projectmod/ProjectMod.java
@@ -1,6 +1,8 @@
 package net.igor.projectmod;
 
 import com.mojang.logging.LogUtils;
+import net.igor.projectmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -22,6 +24,7 @@ public class ProjectMod
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
@@ -35,7 +38,9 @@ public class ProjectMod
 
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.DRYROOT);
+        }
     }
 
     @SubscribeEvent
---
+++ src/main/java/net/igor/projectmod/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.igor.projectmod.item;
+
+import net.igor.projectmod.ProjectMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, ProjectMod.MOD_ID);
+
+    public static final RegistryObject<Item> DRYROOT = ITEMS.register("dryroot",
+            () -> new Item(new Item.Properties()));
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+}
